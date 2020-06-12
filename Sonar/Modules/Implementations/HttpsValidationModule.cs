71bd04378a2e0be971fdbb073b7af63ed75db34d
ramonaerts <ramon.aerts@gmail.com>
2020-06-12 15:06:59 +0200

    https validation working if ssl is good

---
+++ Sonar/Modules/Implementations/HttpsValidationModule.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sonar.Modules.Implementations
+{
+    public class HttpsValidationModule : WebServerModule
+    {
+        private readonly string _host;
+        private X509Certificate2 _certificate;
+        private List<string> _certificateResults = new List<string>();
+
+        public override string Name { get; set; } = "HTTPS Validation";
+
+        public HttpsValidationModule(string host) : base(host)
+        {
+            _host = host;
+        }
+
+        public override Task<ModuleResult> Execute(Data data)
+        {
+            //TODO: Change to _host if data isn't hardcoded anymore in program.cs
+            GetCertificate("https://mail.google.com");
+
+            _certificateResults.Add("Valid from: " + _certificate.GetEffectiveDateString());
+            _certificateResults.Add("Valid till: " + _certificate.GetExpirationDateString());
+            _certificateResults.Add("Signature name: " + _certificate.SignatureAlgorithm.FriendlyName);
+
+            var result = _certificateResults.Aggregate(_host, (current, cResult) => current + (Environment.NewLine + cResult));
+
+            return Task.FromResult(ModuleResult.Create(this, ResultType.Success, result));
+        }
+
+        private void GetCertificate(string host)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(host);
+            request.ServerCertificateValidationCallback += ServerCertificateValidationCallback;
+            request.GetResponse();
+        }
+
+        private bool ServerCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslpolicyerrors)
+        {
+            _certificate = new X509Certificate2(certificate);
+            return sslpolicyerrors == SslPolicyErrors.None;
+        }
+    }
+}
