1448d31ac97077a85394f5e5400a150509d4eaa4
Bonev,Bozhidar B.D <b.bonev@student.fontys.nl>
2020-06-13 17:41:37 +0200

    DoS1&2

---
+++ Sonar/Modules/Implementations/DoSModuleOne.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sonar.Modules.Implementations
+{
+    class DoSModuleOne : LocalModule
+    {
+        public override string Name { get; set; } = "DoS Module";
+        private List<Denial> serviceDenials = new List<Denial>();
+        public override Task<ModuleResult> Execute(Data data)
+        {
+            string hostip = data.GetData<string>("IpAddress");
+            CompareServiceData(hostip);
+            string denials = "";
+            if (!serviceDenials.Any())
+            {
+                return Task.FromResult(ModuleResult.Create(this, ResultType.Success, "All hosts' services are up!"));
+            }
+            else
+            {
+                foreach (Denial d in serviceDenials)
+                {
+                    denials += Environment.NewLine + d.IP + " - denials - " + d.ServiceDenialed;
+                }
+                return Task.FromResult(ModuleResult.Create(this, ResultType.Warning, denials));
+            }
+
+        }
+
+        public List<string> GetActiveHostsByIP(string hostIP)
+        {
+            List<string> byPing = new List<string>();
+            bool pingable = false;
+            string[] separateIP = hostIP.Split('.');
+            string subip = separateIP[0] + "." + separateIP[1] + "." + separateIP[2] + ".";
+            using (Ping pinger = new Ping())
+            {
+                for (int i = 1; i < 255; i++)
+                {
+                    string tempip = subip + i;
+                    PingReply reply = pinger.Send(tempip);
+                    pingable = reply.Status == IPStatus.Success;
+                    if (pingable)
+                    {
+                        byPing.Add(tempip);
+                    }
+                }
+            }
+
+            return byPing;
+        }
+
+
+        private bool TryConnectTcp(string ipAddress, int port)
+        {
+            using (TcpClient tcpClient = new TcpClient())
+            {
+                try
+                {
+                    if (tcpClient.ConnectAsync(ipAddress, port).Wait(2000))
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+
+                return false;
+            }
+        }
+
+
+
+        public List<string> GetActiveHostsByTCP(string hostIP)
+        {
+            List<string> byTCP = new List<string>();
+            int[] commonports = new int[3] { 25, 80, 443 };
+            string[] separateIP = hostIP.Split('.');
+            string subip = separateIP[0] + "." + separateIP[1] + "." + separateIP[2] + ".";
+            for (int i = 1; i < 255; i++)
+            {
+                for (int j = 0; j < commonports.Length; j++)
+                {
+                    currip = subip + i;
+                    if (TryConnectTcp(currip, commonports[j]))
+                    {
+                        if (!byTCP.Contains(currip))
+                        {
+                            byTCP.Add(currip);
+                        }
+                        
+                    }
+                    else
+                    {
+                        if (byTCP.Contains(currip))
+                        {
+                            serviceDenials.Add(new Denial(currip, " denials tcp on port: " + commonports[j]));
+                        }
+                    }
+                }
+
+            }
+            return byTCP;
+        }
+
+
+
+
+        public void CompareServiceData(string hostIP)
+        {
+            List<string> ipsByPing = GetActiveHostsByIP(hostIP);
+            List<string> ipsByTCP = GetActiveHostsByTCP(hostIP);
+
+
+
+            foreach (string s in ipsByPing)
+            {
+                if (!ipsByTCP.Contains(s))
+                {
+                    serviceDenials.Add(new Denial(s, " Accepts ICMP but not TCP"));
+
+                }
+            }
+            foreach (string s in ipsByTCP)
+            {
+                if (!ipsByPing.Contains(s))
+                {
+                    serviceDenials.Add(new Denial(s, "Accepts TCP but not ICMP"));
+
+                }
+            }
+        }
+    }
+
+    class Denial
+    {
+        public string IP
+        {
+            get { return IP; }
+            set { IP = value; }
+        }
+
+        public string ServiceDenialed
+        {
+            get { return ServiceDenialed; }
+            set { ServiceDenialed = value; }
+        }
+
+        public Denial(string ip, string service)
+        {
+            this.IP = ip;
+            this.ServiceDenialed = service;
+        }
+    }
+}
+
---
+++ Sonar/Modules/Implementations/DoSModuleTwo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Net;
+
+namespace Sonar.Modules.Implementations
+{
+    class DoSModuleTwo : LocalModule
+    {
+        public override string Name { get; set; } = "DoSModule2 - Network speed";
+
+        public override Task<ModuleResult> Execute(Data data)
+        {
+            string hostip = data.GetData<string>("IpAddress");
+            return Task.FromResult(ModuleResult.Create(this, ResultType.Success, CheckInternetSpeed().ToString()));
+
+        }
+
+        public double CheckInternetSpeed()
+        {
+            // Create Object Of WebClient
+            System.Net.WebClient wc = new System.Net.WebClient();
+
+            //DateTime Variable To Store Download Start Time.
+            DateTime dt1 = DateTime.Now;
+
+            //Number Of Bytes Downloaded Are Stored In ‘data’
+            byte[] data = wc.DownloadData("http://google.com");
+
+            //DateTime Variable To Store Download End Time.
+            DateTime dt2 = DateTime.Now;
+
+            //To Calculate Speed in Kb Divide Value Of data by 1024 And Then by End Time Subtract Start Time To Know Download Per Second.
+            return Math.Round((data.Length / 1024) / (dt2 - dt1).TotalSeconds, 2);
+        }
+    }
+}
+
