738c2295605c94a41109bffd96eb31ef9db3f117
Petrov,Stoyan S.I <stoyan.petrov@student.fontys.nl>
2020-06-17 14:55:09 +0300

    adding part 2

---
+++ Sonar 2/Sonar2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Sonar2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHostName = new System.Windows.Forms.TextBox();
+            this.cmbServer = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtxResponse = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtHostName
+            // 
+            this.txtHostName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHostName.Location = new System.Drawing.Point(91, 140);
+            this.txtHostName.Name = "txtHostName";
+            this.txtHostName.Size = new System.Drawing.Size(226, 27);
+            this.txtHostName.TabIndex = 0;
+            // 
+            // cmbServer
+            // 
+            this.cmbServer.FormattingEnabled = true;
+            this.cmbServer.Items.AddRange(new object[] {
+            "whois.internic.net"});
+            this.cmbServer.Location = new System.Drawing.Point(115, 574);
+            this.cmbServer.Name = "cmbServer";
+            this.cmbServer.Size = new System.Drawing.Size(189, 24);
+            this.cmbServer.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(91, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 130);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtxResponse
+            // 
+            this.txtxResponse.Location = new System.Drawing.Point(416, 12);
+            this.txtxResponse.Multiline = true;
+            this.txtxResponse.Name = "txtxResponse";
+            this.txtxResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtxResponse.Size = new System.Drawing.Size(426, 631);
+            this.txtxResponse.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(87, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter URL:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(854, 655);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtxResponse);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbServer);
+            this.Controls.Add(this.txtHostName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Sonar 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHostName;
+        private System.Windows.Forms.ComboBox cmbServer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtxResponse;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
